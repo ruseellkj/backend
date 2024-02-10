818790857c5b85ee9e5a79194b4cbc3084cf4744
Rushil <rkjjariwala@gmail.com>
2024-02-10 17:09:29 +0530

    Add Subscription model

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -39,7 +39,7 @@ const userSchema = new mongoose.Schema(
     watchHistory: [
       {
         type: mongoose.Schema.Types.ObjectId,
-        ref: "video",
+        ref: "Video",
       },
     ],
     refreshToken: {
